a0baf3e079f36311f72a74a498a5798f4d260902
Dominik-Matczak <dm.matczak@gmail.com>
2025-08-01 01:31:23 +0200

    Changes for kind filtering

--- src/components/PageContent.jsx
+++ src/components/PageContent.jsx
@@ -1,13 +1,14 @@
 import '../styles/PageContent.scss'
 import ProductCard from './ProductCard'
 import '../styles/container.scss'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 
 export default function PageContent({ books, loading, selectedKind, setSelectedKind, basket, setBasket }) {
 
     const [numberOfPage, setNumberOfPage] = useState(0);
+    const [ sortedBooks, setSortedBooks ] = useState([])
 
     function handlePageDecrease() {
         
@@ -23,26 +24,42 @@ export default function PageContent({ books, loading, selectedKind, setSelectedK
         } 
     }
 
+    const handleKindChange = (e) => {
+        setSelectedKind(e.target.value);
+    }
+
+    useEffect(() => {
+       setSortedBooks(books)
+
+        if (selectedKind !== "") {
+            const filteredbooks = books.filter(book => book.kind === selectedKind);
+            setSortedBooks(filteredbooks);
+        }
+
+    },[books, selectedKind])
 
     return (
-
+        
         loading ? <h2>Trwa wczytywanie książek</h2> : 
-        
 
-        
         <div className="container" style={{display: 'flex', flexDirection: 'column'}}>
             <div className="page-products">
                 <div className="products-filtering">
                     <p>Gatunek</p>
-                    <input type="radio" name="kind" value="Liryka" onChange={e => setSelectedKind(e.target.value)} /> Liryka
-                    <input type="radio" name="kind" value="Epika" onChange={e => setSelectedKind(e.target.value)}  /> Epika
-                    <input type="radio" name="kind" value="Dramat" onChange={e => setSelectedKind(e.target.value)} /> Dramat
+                    <div className="kind-filtering">              
+                    <select id='kind-select' name="kind" onChange={handleKindChange} value={selectedKind}>
+                        <option hidden value="">Wybierz gatunek</option>
+                        <option value="Liryka">Liryka</option>
+                        <option value="Epika">Epika</option>
+                        <option value="Dramat">Dramat</option>
+                    </select>
+                    {selectedKind !== "" && <button onClick={() => setSelectedKind("")}>Wyczyść filtr</button>}
+                    </div>
                 </div>
                 <div className="products">
 
-                {books.map((book, index) => {
-
-                    if (index >= 50 * numberOfPage && index <= (50 * (numberOfPage + 1) - 1)) {   
+                {sortedBooks.map((book, index) => {
+                    if (index >= 25 * numberOfPage && index <= (25 * (numberOfPage + 1) - 1)) {   
                         return <ProductCard book={book} key={index} basket={basket} setBasket={setBasket}/>
                     }
                 })}
@@ -51,7 +68,7 @@ export default function PageContent({ books, loading, selectedKind, setSelectedK
             </div>
             <div className='page-buttons-container'>
                 {numberOfPage > 0 && <button className='page-button' onClick={handlePageDecrease}>Poprzednia strona</button>}
-                {numberOfPage <= (Math.floor(books.length / 50) - 1) && <button className='page-button' onClick={handlePageIncrease}>Następna strona</button>}
+                {numberOfPage <= (Math.floor(sortedBooks.length / 50) - 1) && <button className='page-button' onClick={handlePageIncrease}>Następna strona</button>}
             </div>
         </div>
 
